3224e2f9de8c7729dd11426d169ad94e0f0811fd
cema <cema@itu.dk>
2022-10-26 14:12:49 +0200

    Setup router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './Pages/Home';
+import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <nav>
+          <ul>
+            <li><Link to="/">Sign in</Link></li>
+            <li><Link to="signup">Sign Up</Link></li>
+            <li><Link to="home">Home</Link></li>
+          </ul>
+        </nav>
+      </div>
+
+      <Routes>
+        <Route path= "/" element = {<SignIn/>}/>
+        <Route path="signup" element = {<SignUp/>}/>
+        <Route path="home" element = {<Home/>}/>
+      </Routes>
+      
+  </BrowserRouter>
   );
 }
 
