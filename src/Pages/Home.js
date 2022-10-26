a66bf8761bd62e7a82603d36d4825c998e11d559
scpe <scpe@itu.dk>
2022-10-26 15:52:30 +0200

    minor changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import './Home.css';
 
-
-export default function Home() {
+export default () => {
     return (
         <div className='home'>
+        <h1 className="page">Home page</h1>
             <div className='navBar'>
-
+                this is navBar
             </div>
             <div className='box'>
                 <div className='userBox'>
-
+                    this is userBox
                 </div>
                 <div className='chatOverview'>
                     <div className='chat'>
-
+                        this is chat 1
                     </div>
                     <div className='chat'>
-                        
+                        this is chat 2
                     </div>
                     <div className='chat'>
-                        
+                        this is chat 3
                     </div>
                     <div className='newChats'>
                         <div className='newChat'>
-
+                            this is new chat
                         </div>
                         <div className='newGroupChat'>
-
+                            this is new group chat
                         </div>
                     </div>
                 </div>
