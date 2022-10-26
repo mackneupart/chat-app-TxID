9844db20a7ae8af824b04a2a5f82aeb35623fc0e
scpe <scpe@itu.dk>
2022-10-26 14:45:45 +0200

    basic shapes of the elements

---
+++ src/Pages/Home.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Home.css';
+
+
+export default function Home() {
+    return (
+        <div className='home'>
+            <div className='navBar'>
+
+            </div>
+            <div className='box'>
+                <div className='userBox'>
+
+                </div>
+                <div className='chatOverview'>
+                    <div className='chat'>
+
+                    </div>
+                    <div className='chat'>
+                        
+                    </div>
+                    <div className='chat'>
+                        
+                    </div>
+                    <div className='newChats'>
+                        <div className='newChat'>
+
+                        </div>
+                        <div className='newGroupChat'>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
