58dff7269e808597e33191b884e945b06fa4f00b
KSEHano <khano@uos.de>
2022-10-31 15:27:29 +0100

    Added structure for left side of signUp

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -2,13 +2,18 @@
 import LanguageDropdown from "../Components/language/LangugageDropdown";
 import TextInput from "../Components/text/TextInput";
 
-//Save this input somewhere
+//TODO: Save this input somewhere
 export default function SignUp(){
 
     //const alert = useAlert()
     //TODO: Handle Submit
     function handleSubmit(){
         console.log("Submitted!")
+    }
+    //const profilePicture = [picture, setPicture]
+    function handleSelect(picture){
+        //change profile picture to selected picture
+
     }
 
     
@@ -17,6 +22,22 @@ export default function SignUp(){
         <form onSubmit={handleSubmit}>
             <h1 className="page">Sign UP Page</h1>
             <div>
+                {/**left side, picture */}
+                <div name = "lila_box">
+                    <div name = "profilePic">
+
+                    </div>
+                    <label>Select a profile picture:</label>
+                    <div name = "pictureSelection">
+
+                    </div>
+                    <input type="button" value = "Select" onClick = {() => handleSelect(/**picture */)}></input>
+
+                </div>
+            </div>
+            <div>
+                {/**right side user information */}
+                {/**User name control */}
                 <label>Username:</label> <TextInput/> <br/>
                 <label>E-mail :</label> <TextInput/><br/>
                 {/** TODO passwort control*/}
@@ -30,7 +51,7 @@ export default function SignUp(){
                 <LanguageDropdown/> <br/>
                 <label>What are your interests:</label> <input type="text"></input>
             </div><br/>
-            <input type="submit"></input>
+            <input type="submit" value = "Sign Up"></input>
         </form>
     )
 }
