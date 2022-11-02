ea5b15d7fadb0c5c920a5fc2b591e68d791a23e6
scpe <scpe@itu.dk>
2022-11-02 15:51:39 +0100

    add user info placeholder text

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,39 +1,37 @@
-import React from 'react';
-import './Home.css';
+import React from "react";
+import { IconContext } from "react-icons";
+import { IoLogoOctocat } from "react-icons/io5";
+import "./Home.css";
 
 export default () => {
-    return (
-        <div className='home'>
-        <h1 className="page">Home page</h1>
-            <div className='navBar'>
-                this is navBar
-            </div>
-            <div className='box'>
-                <div className='userBox'>
-                    this is userBox
-                </div>
-                <div className='chatOverview'>
-                    <div className='chat'>
-                        this is chat 1
-                    </div>
-                    <div className='chat'>
-                        this is chat 2
-                    </div>
-                    <div className='chat'>
-                        this is chat 3
-                    </div>
-                    <div className='newChats'>
-                        <div className='newChat'>
-                            this is new chat
-                        </div>
-                        <div className='newGroupChat'>
-                            this is new group chat
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="home">
+      <h1 className="page">Home page</h1>
+      <div className="box">
+        <div className="userBox">
+          <IconContext.Provider value={{ className: "circle" }}>
+            <IoLogoOctocat />
+          </IconContext.Provider>
+          <div className="userInfo">
+            <div className="userInfoDetail">Username</div>
+            <div className="userInfoPlaceholder">Bob the Cat</div>
+            <div className="userInfoDetail">Target Language</div>
+            <div className="userInfoPlaceholder">Español/English</div>
+            <div className="userInfoDetail">Native Language</div>
+            <div className="userInfoPlaceholder">Dansk</div>
+          </div>
+          <button className="settingsButton">Settings</button>
         </div>
-    )
-}
-
-
+        <div className="chatOverview">
+          <div className="chat">this is chat 1</div>
+          <div className="chat">this is chat 2</div>
+          <div className="chat">this is chat 3</div>
+          <div className="newChats">
+            <div className="newChat">this is new chat</div>
+            <div className="newGroupChat">this is new group chat</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
