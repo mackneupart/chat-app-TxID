cad9ff136bf745b03b6cdec5a3e61dbd47f1b35f
KSEHano <khano@uos.de>
2022-11-02 16:27:52 +0100

    added pictures

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -2,6 +2,7 @@
 import LanguageDropdown from "../Components/language/LangugageDropdown";
 import TextInput from "../Components/text/TextInput";
 
+
 //TODO: Save this input somewhere
 export default function SignUp(){
 
@@ -13,6 +14,7 @@ export default function SignUp(){
     //const profilePicture = [picture, setPicture]
     function handleSelect(picture){
         //change profile picture to selected picture
+        //<ProfilePic scr = new scr)
 
     }
 
@@ -25,11 +27,25 @@ export default function SignUp(){
                 {/**left side, picture */}
                 <div ClassName = "lila_box">
                     <div Classname = "profilePic">
-
+                        <img className="profilePicture" id="ProfilePicture" alt = "Profile Picture" src = "./CatIcons/cat1.png" />
                     </div>
                     <label>Select a profile picture:</label>
                     <div ClassName = "pictureSelection">
-
+                    <img alt="Cat1" src="./CatIcons/cat1.png" />
+                    <img alt="Cat2" src="./CatIcons/cat2.png" />
+                    <img alt="Cat3" src="./CatIcons/cat3.png" />
+                    <img alt="Cat4" src="./CatIcons/cat4.png" />
+                    <img alt="Cat5" src="./CatIcons/cat5.png" />
+                    <img alt="Cat6" src="./CatIcons/cat6.png" />
+                    <img alt="Cat7" src="./CatIcons/cat7.png" />
+                    <img alt="Cat8" src="./CatIcons/cat8.png" />
+                    <img alt="Cat9" src="./CatIcons/cat9.png" />
+                    <img alt="Cat10" src="./CatIcons/cat10.png" />
+                    <img alt="Cat11" src="./CatIcons/cat11.png" />
+                    <img alt="Cat12" src="./CatIcons/cat12.png" />
+                    <img alt="Cat13" src="./CatIcons/cat13.png" />
+                    <img alt="Cat14" src="./CatIcons/cat14.png" />
+                    <img alt="Cat15" src="./CatIcons/cat15.png" />
                     </div>
                     <input type="button" value = "Select" onClick = {() => handleSelect(/**picture */)}></input>
 
