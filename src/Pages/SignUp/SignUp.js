cead276b12b32e33f2b2d5aef85f3770bee1253b
cema <cema@itu.dk>
2022-11-02 16:29:19 +0100

    Edit classnames and divs

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,6 +1,7 @@
 
 import LanguageDropdown from "../../Components/language/LangugageDropdown";
 import TextInput from "../../Components/text/TextInput";
+import "./SignUp.css";
 
 //TODO: Save this input somewhere
 export default function SignUp(){
@@ -18,40 +19,43 @@ export default function SignUp(){
     
     return(
     
-        <form onSubmit={handleSubmit}>
-            <h1 className="page">Sign UP Page</h1>
-            <div>
-                {/**left side, picture */}
-                <div ClassName = "lila_box">
-                    <div Classname = "profilePic">
+        <div>
+            <div className="background">
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        {/**left side, picture */}
+                        <div ClassName = "lila_box">
+                            <div Classname = "profilePic">
 
+                            </div>
+                            <label>Select a profile picture:</label>
+                            <div ClassName = "pictureSelection">
+
+                            </div>
+                            <input type="button" value = "Select" onClick = {() => handleSelect(/**picture */)}></input>
+
+                        </div>
                     </div>
-                    <label>Select a profile picture:</label>
-                    <div ClassName = "pictureSelection">
-
+                    <div>
+                        {/**right side user information */}
+                        {/**User name control */}
+                        <label>Username:</label> <TextInput/> <br/>
+                        <label>E-mail :</label> <TextInput/><br/>
+                        {/** TODO password control*/}
+                        <label>Password: </label><input type="password"></input><br/>
+                        {/**repeat passwotd */}
                     </div>
-                    <input type="button" value = "Select" onClick = {() => handleSelect(/**picture */)}></input>
-
-                </div>
+                    <div>
+                        {/**TODO: show different Languages /intersts after selection */}
+                        <label>What is your native Language</label>
+                        <LanguageDropdown/> <br/>
+                        <label>What languages do you want to learn?</label>
+                        <LanguageDropdown/> <br/>
+                        <label>What are your interests:</label> <input type="text"></input>
+                    </div><br/>
+                    <input type="submit" value = "Sign Up"></input>
+                </form>
             </div>
-            <div>
-                {/**right side user information */}
-                {/**User name control */}
-                <label>Username:</label> <TextInput/> <br/>
-                <label>E-mail :</label> <TextInput/><br/>
-                {/** TODO password control*/}
-                <label>Password: </label><input type="password"></input><br/>
-                {/**repeat passwotd */}
-            </div>
-            <div>
-                {/**TODO: show different Languages /intersts after selection */}
-                <label>What is your native Language</label>
-                <LanguageDropdown/> <br/>
-                <label>What languages do you want to learn?</label>
-                <LanguageDropdown/> <br/>
-                <label>What are your interests:</label> <input type="text"></input>
-            </div><br/>
-            <input type="submit" value = "Sign Up"></input>
-        </form>
+        </div>
     )
 }
