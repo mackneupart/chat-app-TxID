71f5fa9b053185e1ebace18f0a275a8f047970da
macn <macn@itu.dk>
2022-11-02 13:37:35 +0100

    adding form tag to input

--- src/Pages/SignIn.js
+++ src/Pages/SignIn.js
@@ -1,5 +1,5 @@
 
-import React from "react"
+import React, { useState } from "react"
 import {
     BrowserRouter,
     Routes,
@@ -9,6 +9,10 @@ import {
 import SignUp from './SignUp';
 
 function SignIn(){
+
+    const[username,setUserName] = useState("");
+
+
     return (
         <div>
             <div className="background">
@@ -18,17 +22,17 @@ function SignIn(){
                         <img src="./Icons/welcome-cat.png"/>
                         <h1 className="header-welcome">WELCOME</h1>
                     </div>
+                    <form>
+                        <div className="input-container">
+                        <input className="input-field" type="text" placeholder="username"/>
+                        <img src="./Icons/welcome-user-90.png" width="30px" className="input-logo"/>
+                        </div>
 
-                    <div className="input-container">
-                    <input className="input-field" type="text" placeholder="username"/>
-                    <img src="./Icons/welcome-user-90.png" width="30px" className="input-logo"/>
-                    </div>
-
-                    <div className="input-container">
-                    <input className="input-field" type="text" placeholder="password"/>
-                    <img src="./Icons/welcome-lock.png" width="30px" className="input-logo"/>
-                    </div>
-
+                        <div className="input-container">
+                        <input className="input-field" type="text" placeholder="password"/>
+                        <img src="./Icons/welcome-lock.png" width="30px" className="input-logo"/>
+                        </div>
+                    </form>
                     <a href="http://google.com">Forgot password?</a>
                     <button className="button-default button-logIn"><Link className="button-text" to="home">Login</Link></button>
                     <button className="button-default button-singUp"><Link className="button-text" to="signUp">Sign Up</Link></button>
