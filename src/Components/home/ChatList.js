c3925a3139faec0fc72a732a0d813b62ac02dd09
scpe <scpe@itu.dk>
2022-11-07 06:47:18 +0100

    add condition based on type of chat

--- src/Components/home/ChatList.js
+++ src/Components/home/ChatList.js
@@ -1,14 +1,21 @@
 import React from "react";
 import ChatListItem from "./ChatListItem";
+import ChatListGroupItem from "./ChatListGroupItem";
 import "./ChatList.css"
 
 const ChatList = ({ chatList }) => {
   return (
     <div className="chat-list">
       {chatList.map((chat) => {
-        return <ChatListItem chat={chat} />;
+        if (chat.hasOwnProperty("chat")) {
+          console.log(chat.hasOwnProperty("chat"));
+          return <ChatListItem chat={chat} />;
+        } else if (chat.hasOwnProperty("groupChat")) {
+          console.log(chat.hasOwnProperty("groupChat"));
+          return <ChatListGroupItem {...chat} />;
+        }
       })}
-    </div>
+    </div> 
   );
 };
 
