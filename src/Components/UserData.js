912fd8bde6d2ea12674cb4dcfb8cb359d9c7ed37
scpe <scpe@itu.dk>
2022-11-07 01:31:12 +0100

    create dummy user data

---
+++ src/Components/UserData.js
@@ -0,0 +1,9 @@
+const UserData = [
+    {id:"01", username:"Bob the Cat", TL:"Spanish", NL:"Danish", image:"https://cdn2.thecatapi.com/images/lm.jpg"},
+    {id:"02", username:"Mewter", TL:"Danish", NL:"Spanish", image:"https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&quality=85&auto=format&fit=max&s=a52bbe202f57ac0f5ff7f47166906403"},
+    {id:"03", username:"Catniss", TL:"Danish", NL:"Spanish", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmle0ZcE4jDG1WkSAhdx9mdTovQFmEq40vOf62Qkz--wz5UiU-kP_75JC8pPZHcXOgg1g&usqp=CAU"},
+    {id:"04", username:"Kitty", TL:"Spanish", NL:"Danish", image:"https://www.rd.com/wp-content/uploads/2021/01/GettyImages-1175550351.jpg"},
+    {id:"05", username:"Kat", TL:"Danish", NL:"Spanish", image:"https://i.natgeofe.com/n/3861de2a-04e6-45fd-aec8-02e7809f9d4e/02-cat-training-NationalGeographic_1484324_3x4.jpg"},
+];
+
+export default UserData;
