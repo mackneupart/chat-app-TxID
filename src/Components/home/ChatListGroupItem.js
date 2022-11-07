230e95d6d7af3ccdcd5578fd718fe9a24643b764
scpe <scpe@itu.dk>
2022-11-07 06:45:53 +0100

    create component for groupchats

---
+++ src/Components/home/ChatListGroupItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ChatListGroupItem = (props) => {
+  return (
+    <Link to="/">
+      <div className="chat-list-item-box">
+        <div className="chat-list-group-item-img-box">
+          <img className="chat-list-item-img" src={props.groupChat[0].image}  />
+          <img className="chat-list-item-img" src={props.groupChat[1].image}  />
+        </div>
+        <div className="chat-list-item-info">
+          <div className="chat-list-item-info-name">{props.groupChat[0].interest}</div>
+          <div className="chat-list-item-info-language">
+             {props.groupChat[0].TL}/ {props.groupChat[0].NL}
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default ChatListGroupItem;
+ 
