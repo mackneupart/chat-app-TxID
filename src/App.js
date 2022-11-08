ec9207f88ff73323547ac52b6d6954e80f6a04be
cema <cema@itu.dk>
2022-11-08 17:14:16 +0100

    Connect our app to Back4App database

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ import {
 import Home from './Pages/Home';
 import SignIn from './Pages/SignIn/SignIn.js';
 import SignUp from './Pages/SignUp/SignUp';
+import Parse from 'parse/dist/parse.min.js';
+
+const PARSE_APPLICATION_ID = 'oyvNGU2jDF7KZjkNpZCyXZh9xpBaIh0Mrzg6fwpg';
+const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
+const PARSE_JAVASCRIPT_KEY = 'Zymf5X99QaEaZWjjWF8f3TVCtfi0SCWHdW7kjyU4';
+Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
+Parse.serverURL = PARSE_HOST_URL;
 
 function App(props) {
   return (
