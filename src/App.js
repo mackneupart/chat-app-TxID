0edfa002e7ebf70e301f119f49386d6af4fdecfe
cema <cema@itu.dk>
2022-11-16 15:58:51 +0100

    Get keys from .env file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import "./App.css";
 import "./Design System/colours.css";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import SignIn from "./Pages/SignIn/SignIn.js";
 import SignUp from "./Pages/SignUp/SignUp";
 import Parse from "parse/dist/parse.min.js";
 
-const PARSE_APPLICATION_ID = "oyvNGU2jDF7KZjkNpZCyXZh9xpBaIh0Mrzg6fwpg";
-const PARSE_HOST_URL = "https://parseapi.back4app.com/";
-const PARSE_JAVASCRIPT_KEY = "Zymf5X99QaEaZWjjWF8f3TVCtfi0SCWHdW7kjyU4";
-Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
-Parse.serverURL = PARSE_HOST_URL;
+const ParseAppID = process.env.REACT_APP_PARSE_APPLICATION_ID;
+const ParseHostURL = process.env.REACT_APP_PARSE_HOST_URL;
+const ParseJavaScriptKey = process.env.REACT_APP_PARSE_JAVASCRIPT_KEY;
+Parse.initialize(ParseAppID, ParseJavaScriptKey);
+Parse.serverURL = ParseHostURL;
 
 function App(props) {
   return (
