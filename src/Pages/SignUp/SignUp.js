6537a20f0676283d9de12745a4576fe17f976236
KSEHano <khano@uos.de>
2022-11-22 12:52:40 +0100

    picture only get fetched once

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -3,7 +3,7 @@ import TextInput from "../../Components/text/TextInput";
 import InterestList from "../../Components/InterestList/InterestList"
 import "./SignUp.css";
 import Parse from 'parse/dist/parse.min.js';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import { Navigate,useNavigate } from "react-router-dom";
 
 export default function SignUp(){
@@ -34,21 +34,23 @@ export default function SignUp(){
         }
       };
     
-    const getCatPNG = async function(){
-        const query = new Parse.Query("catIcons");
-        try{
+    useEffect(()=> {
+        const getCatPNG = async function(){
+            const query = new Parse.Query("catIcons");
             const icons = await query.find();
             setCatIcons(icons)
             
-        }catch (error) {
-            // Error can be caused by wrong parameters or lack of Internet connection
-            return false;
-          }
-        
-    }
+        }
+        getCatPNG()
+            .catch(console.error);
+
+    },[])
+    
+
+
     function makeProfileSelection(){
         //to avoid many reloads
-        if (catIcons !== null){
+        
             try{
                 return ( 
                     <div>
@@ -60,33 +62,10 @@ export default function SignUp(){
                 );
             } catch (error){
                 return false;
-            }
-        }
-       
-        
-        
+            }        
     }
 
-    getCatPNG();
-    /**
-    const src = [
-        "./CatIcons/cat1.png", 
-        "./CatIcons/cat2.png" ,
-        "./CatIcons/cat3.png", 
-        "./CatIcons/cat4.png", 
-        "./CatIcons/cat5.png", 
-        "./CatIcons/cat6.png", 
-        "./CatIcons/cat7.png", 
-        "./CatIcons/cat8.png", 
-        "./CatIcons/cat9.png" ,
-        "./CatIcons/cat10.png",
-        "./CatIcons/cat11.png",
-        "./CatIcons/cat12.png",
-        "./CatIcons/cat13.png",
-        "./CatIcons/cat14.png",
-        "./CatIcons/cat15.png"
-        ]
-        */
+    
     function handleSelect(source){
         //change profile picture to selected picture
         const profPic = document.getElementById("ProfilePicture");
@@ -106,23 +85,6 @@ export default function SignUp(){
                     <label>Select a profile picture:</label>
                     <div className = "pictureSelection">
                         {makeProfileSelection()}
-                        {/**
-                          <img alt="Cat1" src={src[0]} onClick={() => handleSelect(src[0])} />
-                        <img alt="Cat2" src={src[1]} onClick={() => handleSelect(src[1])}/>
-                        <img alt="Cat3" src={src[2]} onClick={() => handleSelect(src[2])}/>
-                        <img alt="Cat4" src={src[3]} onClick={() => handleSelect(src[3])}/>
-                        <img alt="Cat5" src={src[4]} onClick={() => handleSelect(src[4])}/>
-                        <img alt="Cat6" src={src[5]} onClick={() => handleSelect(src[5])}/>
-                        <img alt="Cat7" src={src[6]} onClick={() => handleSelect(src[6])}/>
-                        <img alt="Cat8" src={src[7]} onClick={() => handleSelect(src[7])}/>
-                        <img alt="Cat9" src={src[8]} onClick={() => handleSelect(src[8])}/>
-                        <img alt="Cat10" src={src[9]} onClick={() => handleSelect(src[9])} />
-                        <img alt="Cat11" src={src[10]} onClick={() => handleSelect(src[10])} />
-                        <img alt="Cat12" src={src[11]} onClick={() => handleSelect(src[11])} />
-                        <img alt="Cat13" src={src[12]} onClick={() => handleSelect(src[12])} />
-                        <img alt="Cat14" src={src[13]} onClick={() => handleSelect(src[13])} />
-                        <img alt="Cat15" src={src[14]} onClick={() => handleSelect(src[14])} />
-                         */}
                        
                     </div>
                     {/** we accidentally deleted the Select button but it might work better without */}
