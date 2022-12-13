0ca03b0cd291fdee13b90ede3e71a206de27122d
macn <macn@itu.dk>
2022-12-13 13:06:01 +0100

    less hover effect on chat list items

--- src/Pages/Chat/Chat.js
+++ src/Pages/Chat/Chat.js
@@ -2,16 +2,15 @@
 import { Navigate, useNavigate, useLocation } from "react-router-dom";
 import ChatBox from "../../Components/ChatBox/ChatBox";
 import ChatSidebar from "../../Components/ChatSidebar/ChatSidebar";
-import "./Chat.css"
+import "./Chat.css";
 // import{readChats2} from "../../API/API"
 
 export default function Chat() {
   const { state } = useLocation();
   const currentUser = state.currentUser;
   const otherUser = state.otherUser;
-  
-  //const navigate = useNavigate();
 
+  //const navigate = useNavigate();
 
   return (
     // <div className="chat-page">
@@ -24,11 +23,15 @@ export default function Chat() {
     //   <ChatBox classname = "chat-box" currentUser={currentUser} otherUser={otherUser} />
     // </div>
     <div className="chat-page">
-        <div className="chat-sidebar">
-          <ChatSidebar currentUser = {currentUser}/>
-        </div>
-        
-        <ChatBox className = "chat-box" currentUser={currentUser} otherUser = {otherUser}/>
+      <div className="chat-sidebar">
+        <ChatSidebar currentUser={currentUser} />
+      </div>
+
+      <ChatBox
+        className="chat-box"
+        currentUser={currentUser}
+        otherUser={otherUser}
+      />
     </div>
   );
-};
+}
