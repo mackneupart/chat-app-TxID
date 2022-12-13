210d959a792bc0f79855c2cbef8e0832a4dd680c
cema <cema@itu.dk>
2022-12-13 13:17:17 +0100

    Edit CSS for topbar in Chat

--- src/Pages/Chat/Chat.js
+++ src/Pages/Chat/Chat.js
@@ -1,7 +1,6 @@
 import { Navigate, useNavigate, useLocation } from "react-router-dom";
 import ChatBox from "../../Components/ChatBox/ChatBox";
 import ChatSidebar from "../../Components/ChatSidebar/ChatSidebar";
-import { logOutUser } from "../../API/API";
 import Button from "../../Components/Button/Button";
 import "./Chat.css"
 
@@ -10,16 +9,6 @@ export default function Chat() {
   const currentUser = state.currentUser;
   const otherUser = state.otherUser;
   const navigate = useNavigate();
-
-  const handleLogout = async function () {
-    try {
-      if (logOutUser()) {
-        navigate("/");
-      }
-    } catch (error) {
-      console.log(`Error when trying to log out user! ${error}`);
-    }
-  };
 
   const goHome = function () {
     navigate("/home", {
@@ -30,7 +19,6 @@ export default function Chat() {
   
   return (
     <div className="chat-page">
-      <div className="topbar">
         <img
             className="home-icon"
             src="./Icons/home.png"
@@ -38,13 +26,11 @@ export default function Chat() {
             onClick={goHome}
           />
         <div className="chat-partner">
-          <img src={otherUser.get("profilePicture").get("catPNG")._url}/>  
+          <img className="partner-pic" src={otherUser.get("profilePicture").get("catPNG")._url}/>  
           {/* We should clean up  the functions for getting profile pic
           in the API !!! /cema */}
           <header className="partner-name">{otherUser.get("username")}</header>
         </div>
-        <Button className="logout-button" text="Log out" click={handleLogout}/>
-      </div>
       <div className="chat-sidebar">
         <ChatSidebar currentUser = {currentUser}/>
       </div>
