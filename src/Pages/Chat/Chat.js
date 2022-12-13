ba5aacd253ddcae6350789f13130038f676cf19a
cema <cema@itu.dk>
2022-12-13 12:22:54 +0100

    Create topbar in Chat page

--- src/Pages/Chat/Chat.js
+++ src/Pages/Chat/Chat.js
@@ -1,34 +1,54 @@
-// import React, { useEffect, useState } from "react";
 import { Navigate, useNavigate, useLocation } from "react-router-dom";
 import ChatBox from "../../Components/ChatBox/ChatBox";
 import ChatSidebar from "../../Components/ChatSidebar/ChatSidebar";
+import { logOutUser } from "../../API/API";
+import Button from "../../Components/Button/Button";
 import "./Chat.css"
-// import{readChats2} from "../../API/API"
 
 export default function Chat() {
   const { state } = useLocation();
   const currentUser = state.currentUser;
   const otherUser = state.otherUser;
+  const navigate = useNavigate();
+
+  const handleLogout = async function () {
+    try {
+      if (logOutUser()) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(`Error when trying to log out user! ${error}`);
+    }
+  };
+
+  const goHome = function () {
+    navigate("/home", {
+      state: {currentUser : currentUser },
+      // The navigation to and from Home needs fixing, screen goes blank but works on refresh. /cema
+    });
+  }
   
-  //const navigate = useNavigate();
-
-
   return (
-    // <div className="chat-page">
-    //   <div className="flex_between">
-    //     <h2 className="list_heading">{`${currentUser.get(
-    //       "username"
-    //     )} sending, ${otherUser.get("username")} receiving!`}</h2>
-    //   </div>
-    //   <ChatSidebar className= "chat-sidebar" chats ={chats} />
-    //   <ChatBox classname = "chat-box" currentUser={currentUser} otherUser={otherUser} />
-    // </div>
     <div className="chat-page">
-        <div className="chat-sidebar">
-          <ChatSidebar currentUser = {currentUser}/>
+      <div className="topbar">
+        <img
+            className="home-icon"
+            src="./Icons/home.png"
+            alt="Home icon"
+            onClick={goHome}
+          />
+        <div className="chat-partner">
+          <img src={otherUser.get("profilePicture").get("catPNG")._url}/>  
+          {/* We should clean up  the functions for getting profile pic
+          in the API !!! /cema */}
+          <header className="partner-name">{otherUser.get("username")}</header>
         </div>
-        
-        <ChatBox className = "chat-box" currentUser={currentUser} otherUser = {otherUser}/>
+        <Button className="logout-button" text="Log out" click={handleLogout}/>
+      </div>
+      <div className="chat-sidebar">
+        <ChatSidebar currentUser = {currentUser}/>
+      </div>
+      <ChatBox className = "chat-box" currentUser={currentUser} otherUser = {otherUser}/>
     </div>
   );
 };
