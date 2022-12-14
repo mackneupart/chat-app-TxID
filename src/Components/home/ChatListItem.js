eecc586b0549a06d273064cd31904fa4fb3b4e98
macn <macn@itu.dk>
2022-12-14 16:02:58 +0100

    adding a deleteChatButton to homePage

--- src/Components/home/ChatListItem.js
+++ src/Components/home/ChatListItem.js
@@ -4,12 +4,13 @@ import "./ChatListItem.css";
 
 const ChatListItem = ({ chat, currentUser }) => {
   const navigate = useNavigate();
-  var otherUser = {}
-  if (chat.get("users2")[0].id === currentUser.id) { otherUser = chat.get("users2")[1]}
-  else { otherUser = chat.get("users2")[0]}
-  const otherUserImage = otherUser
-    .get("profilePicture")
-    .get("catPNG")._url;
+  var otherUser = {};
+  if (chat.get("users2")[0].id === currentUser.id) {
+    otherUser = chat.get("users2")[1];
+  } else {
+    otherUser = chat.get("users2")[0];
+  }
+  const otherUserImage = otherUser.get("profilePicture").get("catPNG")._url;
   const language1 = chat.get("Language1");
   const language2 = chat.get("Language2");
 
@@ -19,15 +20,26 @@ const ChatListItem = ({ chat, currentUser }) => {
     });
   };
 
+  const deleteChat = () => {
+    alert("you pressed deleteChat");
+  };
+
   return (
-    <div className="chat-list-item-box" onClick={handleClick}>
-      <div className="chat-list-item-img-box">
-        <img className="chat-list-item-img" src={otherUserImage} />
-      </div>
-      <div className="chat-list-item-info">
-        <div className="chat-list-item-info-name">{otherUser.get("username")}</div>
-        <div className="chat-list-item-info-language">
-          {language1} / {language2}
+    <div>
+      <button className="delete-chat-button" onClick={deleteChat}>
+        X
+      </button>
+      <div className="chat-list-item-box" onClick={handleClick}>
+        <div className="chat-list-item-img-box">
+          <img className="chat-list-item-img" src={otherUserImage} />
+        </div>
+        <div className="chat-list-item-info">
+          <div className="chat-list-item-info-name">
+            {otherUser.get("username")}
+          </div>
+          <div className="chat-list-item-info-language">
+            {language1} / {language2}
+          </div>
         </div>
       </div>
     </div>
